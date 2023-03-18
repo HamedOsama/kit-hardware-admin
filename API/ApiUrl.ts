2c91566acd0424b48fa97491c2945251ac0f58e3
Hamed Osama <hamedosama19@gmail.com>
2023-03-18 21:31:33 +0200

    add production API keys

--- API/ApiUrl.ts
+++ API/ApiUrl.ts
@@ -1,8 +1,8 @@
 const env = {
-  API_URL: "http://localhost:8000/api/v1",
-  ImageUrl: "http://localhost:8000/images/",
-  // API_URL: "https://api.kit-hardware-center.com/api/v1",
-  // ImageUrl: "https://api.kit-hardware-center.com/images/",
+  // API_URL: "http://localhost:8000/api/v1",
+  // ImageUrl: "http://localhost:8000/images/",
+  API_URL: "https://api.kit-hardware-center.com/api/v1",
+  ImageUrl: "https://api.kit-hardware-center.com/images/",
 }
 // const API_URL = "http://localhost:8000/api/v1";
 // const ImageUrl = "http://localhost:8000/images/";
