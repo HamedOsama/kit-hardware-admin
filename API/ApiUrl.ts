1a3a489131f545217b81323d9e8492e69a7dc897
Hamed Osama <77761661+HamedOsama@users.noreply.github.com>
2023-06-05 04:13:55 +0300

    Update ApiUrl.ts

--- API/ApiUrl.ts
+++ API/ApiUrl.ts
@@ -3,7 +3,7 @@ const env = {
   // ImageUrl: "http://localhost:8000/images/",
   API_URL: "https://api.kit-hardware-center.com/api/v1",
   ImageUrl: "https://api.kit-hardware-center.com/images/",
-}
+};
 // const API_URL = "http://localhost:8000/api/v1";
 // const ImageUrl = "http://localhost:8000/images/";
 // const API_URL = "https://hatly-server.onrender.com/api/v1";
